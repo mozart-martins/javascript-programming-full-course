2ab857eb516e4f6d3ace34cdd7227569d0ad5e48
Mozart Dias Martins <mozartmartins@mpto.mp.br>
2022-01-20 19:29:18 -0300

    Começando o jogo de pedra, papel e tesoura

---
+++ largeCountries.js
@@ -0,0 +1,15 @@
+let largeCountries = ["Tuvalu", "Índia", "USA", "Indonésia", "Monaco"]
+
+//Removendo Tuvalu
+largeCountries.shift()
+
+//Removendo Monaco
+largeCountries.pop()
+
+//Adicionando China
+largeCountries.unshift("China")
+
+//Adicionando Paquistão
+largeCountries.push("Paquistão")
+
+console.log(largeCountries)
---
+++ rockPaperScissors.js
@@ -0,0 +1,14 @@
+let hands = ["rock", "paper", "scissors"]
+
+function getHand() {
+    id = Math.floor(Math.random() * 3)
+    return hands[id]
+}
+
+
+let playerOne = getHand()
+let playerTwo = getHand()
+
+function getWinner() {
+    
+}
